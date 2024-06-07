1ab1877fccbf382dbfd41d4c55216d72a8121f17
Orion <mendozagarcia@uninorte.edu.co>
2024-06-06 19:35:50 -0500

    Get all students

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
 
+  const [estudiantes, setEstudiantes] = useState([])
+
+  const get_all_estudiantes = async () => {
+    const peticion = await fetch('http://localhost:3000/students/')
+    const data = await peticion.json()
+    setEstudiantes(data)
+  }
+
+  useEffect(() => {
+    get_all_estudiantes()
+  },[])
+
+  console.log(estudiantes)
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {
+        estudiantes.map(({id, name, age, grades}) => (
+          <div key={id}>
+            <h1>{name}</h1>
+            <h3>Edad: {age}</h3>
+            <h3>Notas:</h3>
+            <div class="infoNotas">
+            {
+              grades.map(({semester, grade, id }) => (
+                <div key= {id} class="notas">
+                  <h4> Semestre : {semester}</h4>
+                  <h4> Nota: {grade} </h4>
+                </div>
+              )) 
+            }
+            </div>
+            <button id={id}>Editar</button>
+          </div>
+        ))
+      }
     </>
-  )
+  )  
 }
-
-export default App
